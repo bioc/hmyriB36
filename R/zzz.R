a7c4b3518b84e480231f2f090b06244b3dc11432
Vince Carey <stvjc@channing.harvard.edu>
2008-09-12 21:41:44 +0000

    ok
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/hmyriB36@710 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,25 +1,25 @@
-datacache <- new.env(hash=TRUE, parent=emptyenv())
-
-hmyriAmbB36_23a_dbconn <- function() dbconn(datacache) # lexical scope?
-hmyriAmbB36_23a_dbfile <- function() dbfile(datacache)
-
-.onLoad <- function(libname, pkgname)
-{
-    require("methods", quietly=TRUE)
-    ## Connect to the SQLite DB
-    dbfile <- system.file("extdata", "hmyriAmbB36_23a.sqlite", 
-       package=pkgname, lib.loc=libname)
-    assign("dbfile", dbfile, envir=datacache)
-    dbconn <- dbFileConnect(dbfile)
-    assign("dbconn", dbconn, envir=datacache)
-    ## Create the AnnObj instances
-    aae = AnnotationDbi:::addToNamespaceAndExport
-    aae("hmyriAmbB36_23a_dbconn", hmyriAmbB36_23a_dbconn, pkgname)
-    aae("hmyriAmbB36_23a_dbfile", hmyriAmbB36_23a_dbfile, pkgname)
-}
-
-.onUnload <- function(libpath)
-{
-    dbFileDisconnect(hmyriAmbB36_23a_dbconn())
-}
-
+#datacache <- new.env(hash=TRUE, parent=emptyenv())
+#
+#hmyriAmbB36_23a_dbconn <- function() dbconn(datacache) # lexical scope?
+#hmyriAmbB36_23a_dbfile <- function() dbfile(datacache)
+#
+#.onLoad <- function(libname, pkgname)
+#{
+#    require("methods", quietly=TRUE)
+#    ## Connect to the SQLite DB
+#    dbfile <- system.file("extdata", "hmyriAmbB36_23a.sqlite", 
+#       package=pkgname, lib.loc=libname)
+#    assign("dbfile", dbfile, envir=datacache)
+#    dbconn <- dbFileConnect(dbfile)
+#    assign("dbconn", dbconn, envir=datacache)
+#    ## Create the AnnObj instances
+#    aae = AnnotationDbi:::addToNamespaceAndExport
+#    aae("hmyriAmbB36_23a_dbconn", hmyriAmbB36_23a_dbconn, pkgname)
+#    aae("hmyriAmbB36_23a_dbfile", hmyriAmbB36_23a_dbfile, pkgname)
+#}
+#
+#.onUnload <- function(libpath)
+#{
+#    dbFileDisconnect(hmyriAmbB36_23a_dbconn())
+#}
+#
