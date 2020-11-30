6d8b85f348ba1c9dc26c248e77b1be5756b77c09
Lori Shepherd <lori.shepherd@roswellpark.org>
2020-11-30 08:25:13 -0500

    User requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -24,3 +24,9 @@
   packageStartupMessage(paste("available chromosomes are named ",
         paste(selectSome(cn), collapse=", ")))
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.14")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
